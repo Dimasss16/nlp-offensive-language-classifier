0247afed2b8cc1d8cd79438a24198e92a8211abf
Dimasss16 <dima.sukhotin@icloud.com>
2025-08-26 18:49:34 +0400

    Edit file paths in config

--- src/config.py
+++ src/config.py
@@ -1,31 +1,19 @@
-"""
-Configuration file - all constants in one place
-"""
-
-# Dataset
 DATASET_NAME = 'tdavidson/hate_speech_offensive'
 
 # Paths
-RAW_DATA_PATH = 'data/raw/hate_speech.csv'
-TRAIN_PATH = 'data/splits/train.csv'
-VAL_PATH = 'data/splits/val.csv'
-TEST_PATH = 'data/splits/test.csv'
+RAW_DATA_PATH = 'src/data/raw/hate_speech.csv'
+TRAIN_PATH = 'src/data/splits/train.csv'
+VAL_PATH = 'src/data/splits/val.csv'
+TEST_PATH = 'src/data/splits/test.csv'
 # Preprocessed paths (using light preprocessing for TF-IDF)
-PROCESSED_TRAIN_PATH = 'data/processed_light/train.csv'
-PROCESSED_VAL_PATH = 'data/processed_light/val.csv'
-PROCESSED_TEST_PATH = 'data/processed_light/test.csv'
+PROCESSED_TRAIN_PATH = 'src/data/processed_light/train.csv'
+PROCESSED_VAL_PATH = 'src/data/processed_light/val.csv'
+PROCESSED_TEST_PATH = 'src/data/processed_light/test.csv'
 
 # BERT preprocessing paths (minimal cleaning, preserves case)
-BERT_TRAIN_PATH = 'data/processed_bert/train.csv'
-BERT_VAL_PATH = 'data/processed_bert/val.csv'
-BERT_TEST_PATH = 'data/processed_bert/test.csv'
-
-# Model paths
-TFIDF_MODEL_PATH = 'models/tfidf_logreg.pkl'
-
-# Results paths
-GROUND_ZERO_RESULTS = 'results/ground_zero_results.txt'
-BASELINE_RESULTS = 'results/baseline_results.txt'
+BERT_TRAIN_PATH = 'src/data/processed_bert/train.csv'
+BERT_VAL_PATH = 'src/data/processed_bert/val.csv'
+BERT_TEST_PATH = 'src/data/processed_bert/test.csv'
 
 # Class names
 CLASS_NAMES = {
@@ -38,7 +26,3 @@ CLASS_NAMES = {
 TEST_SIZE = 0.15
 VAL_SIZE = 0.15
 RANDOM_STATE = 42
-
-# TF-IDF parameters
-MAX_FEATURES = 10000
-NGRAM_RANGE = (1, 3)
